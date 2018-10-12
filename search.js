14f70d8e5c3e9494aad97dff71096a0fe0b0d22b
leemichelle <34196630+leemichelle@users.noreply.github.com>
2018-10-11 21:10:32 -0700

    Delete search.js

--- search.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-const Search = (props) => (
-  <form onChange = {props.handleSearch}>
-    <input type='search'></input>
-  </form>
-);
-
-export default Search;
